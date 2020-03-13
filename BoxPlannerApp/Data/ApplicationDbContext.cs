7996dff7236b91c7758eb7faceca7a2b98e8d16b
thorning <brian@thornings.dk>
2020-03-13 10:43:30 +0100

    initial commit

---
+++ BoxPlannerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BoxPlannerApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoxPlannerApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ BoxPlannerApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxPlannerApp.Models
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public int CoolWheaterZone { get; set; }  
+    }
+}
